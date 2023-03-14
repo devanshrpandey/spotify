d9fda18f706ab27d9905e36b16c526570678f8b5
devanshpandey <devansh.r.pandey@gmail.com>
2023-03-14 14:34:58 -0700

    made the algorithm actually sort by score and reduced number of API calls

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ from spotipy.oauth2 import SpotifyOAuth
 #
 #
 #EDIT THE FOLLOWING
-typeOfScore = "danceability" #can be replaces with energy, speechiness, acousticness, instrumentalness, liveness, valence, tempo
+typeOfScore = "danceability" #can be replaces with danceability, energy, speechiness, acousticness, instrumentalness, liveness, valence, tempo
 maximum = True #TRUE for maximum danceability / energy etc, FALSE for minimum
-numberOfSongs = 60
+numberOfSongs = 50
 playlistName = "dance 'till we die"
-#
+#  
 #
 #
 
@@ -21,19 +21,18 @@ os.environ["SPOTIPY_REDIRECT_URI"] = "http://localhost:9090"
 scope = "user-library-read user-read-playback-state user-modify-playback-state user-read-currently-playing app-remote-control streaming playlist-read-private playlist-read-collaborative playlist-modify-private playlist-modify-public user-read-playback-position user-top-read user-read-recently-played user-library-read user-library-modify"
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
 
-pls = sp.current_user_playlists()["items"] #get the last 50 playlists
+"""pls = sp.current_user_playlists()["items"] #get the last 50 playlists
 allPlaylists = []
 for pl in pls: 
     allPlaylists.append(pl["id"]) #create an array of all playlist ids
 
 nameScores = {}
 scoreIDs = {}
-
-
+ids = []
+features = []
 
 for playlist in allPlaylists: 
     plTracks = sp.user_playlist_tracks(playlist_id=playlist) 
-    ids = []
 
     for item in plTracks["items"]:
         try:
@@ -41,14 +40,16 @@ for playlist in allPlaylists:
         except:
             continue
     
-    features = sp.audio_features(ids) #looping it is how you get around the 100-track limit for sp. audio_features, but it does call the api once per playlist :/
-    for i in range(len(ids)):
-        
-        score = features[i][typeOfScore]
-        
-        scoreIDs[score] = ids[i]
-        #nameScores[name] = score 
-        #name = (plTracks["items"][i]["track"]["name"])
+idCopy = ids
+while len(idCopy) > 100:
+    features.extend(sp.audio_features(idCopy[:100])) #getting around API limit of 100 tracks per "sp.audio_features"
+    idCopy = idCopy[100:] # removing the first 100 tracks from the copy of "ids" and then looping
+features.extend(sp.audio_features(idCopy)) # putting the final tracks into features
+
+
+for i in range(len(ids)):
+    score = features[i][typeOfScore]
+    scoreIDs[score] = ids[i]
 
 #sorting the dictionary by score:
 keys = list(scoreIDs.keys())
@@ -57,11 +58,10 @@ newScoreIDs = {i: scoreIDs[i] for i in keys}
 
 
 
-
 finalTracks = [] # final X tracks to be put into the playlist
 n = 0
-for score in scoreIDs:
-    finalTracks.append(scoreIDs[score]) # gets the ID from the dictionary and puts it in finaltracks
+for score in newScoreIDs:
+    finalTracks.append(newScoreIDs[score]) # gets the ID from the dictionary and puts it in finaltracks
     n += 1
     if n == numberOfSongs:
         break
@@ -69,8 +69,12 @@ for score in scoreIDs:
 myID = sp.me()["id"]
 
 newPlaylist = (sp.user_playlist_create(myID,playlistName))["id"] # creates a new playlist and gets the id
+sp.playlist_add_items(newPlaylist, finalTracks)"""
 
 
-sp.playlist_add_items(newPlaylist, finalTracks)
+print(sp.audio_features("https://open.spotify.com/track/16su7DgyCM31XdlBn6LMvb?si=09aaeb3070374ae2"))
 
+plTracks = sp.user_playlist_tracks(playlist_id="https://open.spotify.com/playlist/1BSZYATxiGcCjkqgcdEbYN?si=78ccf6d886c84ae9") 
 
+for item in plTracks["items"]:
+    print(sp.audio_features(item["track"]["id"])[0]["danceability"])
