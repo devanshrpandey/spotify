d21a738d284682e47e24af26fca52002f87aa3ee
devanshpandey <devansh.r.pandey@gmail.com>
2023-06-12 17:58:34 -0700

    refactoring

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import os
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
+import random
 
 
-#sets environment variables and allows all permissions
+# sets environment variables and allows all permissions
 os.environ["SPOTIPY_CLIENT_ID"] = "375c8e4a9f8d4243996d256ff41daa62"
 os.environ["SPOTIPY_CLIENT_SECRET"] = "1472274bf789453bbd1ed42c9ee181da"
 os.environ["SPOTIPY_REDIRECT_URI"] = "http://localhost:9090"
@@ -12,19 +13,31 @@ sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
 myID = sp.me()["id"]
 
 
-def getsongIDs(numberOfPLstoSearch=50,user=myID):
+def get_playlist_tracks(username, playlist_id):
+    results = sp.user_playlist_tracks(username, playlist_id)
+    tracks = results["items"]
+    while results["next"]:
+        results = sp.next(results)
+        tracks.extend(results["items"])
+    return tracks
+
+
+def getsongIDs(numberOfPLstoSearch=50, user=myID):
     if numberOfPLstoSearch > 50:
         numberOfPLstoSearch = 50
-        print("Number of playlists searched has been capped at 50 as that is the maximum the Spotify API allows.")
-    pls = sp.user_playlists(user,limit=numberOfPLstoSearch)["items"]
+        print(
+            "Number of playlists searched has been capped at 50 as that is the maximum the Spotify API allows."
+        )
+    pls = sp.user_playlists(user, limit=numberOfPLstoSearch)["items"]
     allPlaylists = []
-    for pl in pls: 
-        allPlaylists.append(pl["id"]) #create an array of all playlist ids
+    for pl in pls:
+        allPlaylists.append(pl["id"])  # create an array of all playlist ids
 
     songIDs = []
-    for playlist in allPlaylists: 
-        plTracks = sp.user_playlist_tracks(playlist_id=playlist) 
-        for item in plTracks["items"]:
+    for playlist in allPlaylists:
+        plTracks = get_playlist_tracks(myID, playlist)
+
+        for item in plTracks:
             try:
                 songIDs.append(item["track"]["id"])
             except:
@@ -32,49 +45,69 @@ def getsongIDs(numberOfPLstoSearch=50,user=myID):
 
     return songIDs
 
-def getSortedScores(ids,typeOfScore,maximum=True):
+
+def getSortedScores(ids, typeOfScore, maximum=True):
     idCopy = ids
 
-    scoreIDmap = {} # creates a dictionary with scores as keys and IDs as values, to later sort
-    features = [] 
+    scoreIDmap = (
+        {}
+    )  # creates a dictionary with scores as keys and IDs as values, to later sort
+    features = []
 
     while len(ids) > 100:
-        features.extend(sp.audio_features(ids[:100])) #getting around API limit of 100 tracks per "sp.audio_features"
-        ids = ids[100:] # removing the first 100 tracks from the copy of "ids" and then looping
-    features.extend(sp.audio_features(ids)) # pushing the final tracks into features
-
+        features.extend(
+            sp.audio_features(ids[:100])
+        )  # getting around API limit of 100 tracks per "sp.audio_features"
+        ids = ids[
+            100:
+        ]  # removing the first 100 tracks from the copy of "ids" and then looping
+    features.extend(sp.audio_features(ids))  # pushing the final tracks into features
 
     for i in range(len(idCopy)):
         score = features[i][typeOfScore]
         scoreIDmap[score] = idCopy[i]
-    #sorting the dictionary by score:
+    # sorting the dictionary by score:
     keys = list(scoreIDmap.keys())
     keys.sort(reverse=maximum)
     sortedScores = {i: scoreIDmap[i] for i in keys}
 
     return sortedScores
 
-def createFinalPlaylist(scores,playlistName,numberOfSongs=50):
-    finalTracks = [] # final X tracks to be put into the playlist
+
+def main(
+    playlistName, typeOfScore, numberOfSongs, plsToSearch=50, maximum=True, user=myID
+):
+    songIDs = getsongIDs(plsToSearch, user=user)
+    scores = getSortedScores(songIDs, typeOfScore=typeOfScore, maximum=maximum)
+    finalTracks = []  # final X tracks to be put into the playlist
     n = 0
-    for score in scores:
-        finalTracks.append(scores[score]) # gets the ID from the dictionary and puts it in finaltracks
-        n += 1
-        if n == numberOfSongs:
-            break
+    finalTracks = list(scores.values())[:numberOfSongs]
 
-    newPlaylist = (sp.user_playlist_create(myID,playlistName)) # creates a new playlist
+    scoreList = list(scores.keys())[:numberOfSongs]
+    random.shuffle(scoreList)
+
+    averageScore = sum(scoreList) / len(scoreList)
+    newPlaylist =  None
+    newPlaylist = sp.user_playlist_create(myID, playlistName)  # creates a new playlist
     sp.playlist_add_items(newPlaylist["id"], finalTracks)
-    return newPlaylist
+    return averageScore, typeOfScore, newPlaylist
 
 
-songIDs = getsongIDs(numberOfPLstoSearch=50)
-# searches last N playlists that are in Your Library (that you've created or liked). Maximum 50.
+PLAYLIST_NAME = "end of an decade; start of an age"
+PLAYLIST_LENGTH = 70  # number of songs in the final playlist
+TYPE_OF_SCORE = "danceability"  # danceability, energy, speechiness, acousticness, instrumentalness, liveness, valence, tempo
+NUMBER_OF_PLAYLISTS_TO_SEARCH = 50  # searches last N playlists that are in Your Library (that you've created or liked). Maximum 50.
+MAXIMUM = True  # if True, then it'll find the songs w/ highest score in the category and vice versa (so "danceability" / True would get the most danceable songs)
+USER = myID  # your Spotify ID
 
-sortedScores = getSortedScores(songIDs,typeOfScore="danceability",maximum=True)
-# type of score can be danceability, energy, speechiness, acousticness, instrumentalness, liveness, valence, tempo
-# if "maximum" is set to True, then it'll find the songs w/ highest score in the category and vice versa (so "danceability" / True would get the most danceable songs)
 
-createFinalPlaylist(sortedScores,playlistName="antidancer",numberOfSongs=50)
-
-#set playlist name and number of songs for the final playlist
+print(
+    main(
+        PLAYLIST_NAME,
+        TYPE_OF_SCORE,
+        PLAYLIST_LENGTH,
+        NUMBER_OF_PLAYLISTS_TO_SEARCH,
+        MAXIMUM,
+        USER,
+    )
+)
